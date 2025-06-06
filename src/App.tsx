cb37a8f1511a2e2a21acd38a4d6752cf1404789a
DargeniusStud18 <yasseradjadi9@gmail.com>
2025-06-06 15:55:19 +0100

    feat: Enable eslint rule for exhaustive dependencies in React hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './context/AuthContext';
